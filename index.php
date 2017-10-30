6a7ae68a472c6df2f7e75f36cf6757b89f080225
Unknown <sdethy@gmail.com>
2017-10-30 12:14:43 +0100

    new file

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>HELLO
+
+
+</head>
+<body><h1>Bonjour <?php echo $_GET['nom']; ?>!</h1></body>
+
+<img src="http://md1.libe.com/photo/1068503-violaine-leroy-liberationjpg.jpg?modified_at=1509307396&width=960" alt="belle image">
+
+
+
+<a href="cats.php">vers page cats</a>
+
+
+</html>
---
+++ var/humans.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+
+<javascript>
+  $nom_de_la_variable = 'Valeur de la variable';
+$president_usa = 'Donald Trump';
+$annee_naissance = 1973;
+$is_raining = false;
+$team_players = array('john','Jeanne','Julien');
+</javascript>
+
+
+    <title>Hi!</title>
+  </head>
+  <body>
+    <h1>Bonjour <?php echo $_GET['nom']; ?>!</h1>
+  </body>
+</html>
